da4d635626b998a40e089d753d8984ffda344a87
quickbarn <barnabas.lee@gmail.com>
2020-09-15 21:08:15 -0700

    button interaction and filtering data complete

--- ReadME.md
+++ ReadME.md
@@ -4,4 +4,13 @@
 
 Initial File creation and read through on instruction document. (create repo, git clone, push new files)
 
-App.js file: partial pseudo coding
+App.js file: partial git add pseudo coding
+
+### 9/14 - 
+
+App.js add in, loop to go through data.js and append to table (adding tr)
+
+### 9/15 -
+
+completed interaction with button, taking input, utilizing input to filter data, showing only filtered data on screen
+
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,8 +4,57 @@ var tableData = data;
 // YOUR CODE HERE!
 
 //create variable to hold values
+var tbody = d3.select("tbody");
 
-//loop through data and place in created variable (line6)
+console.log(tableData);
 
 //append from loop into list/array
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 
+
+// select button, button id = <name>
+var button = d3.select("#filter-btn");
+
+// select input field, input class = <name>
+var inputField = d3.select("#form-control");
+
+button.on("click", filterClick);
+inputField.on("submit", filterClick);
+
+// This function is triggered when the button is clicked
+function filterClick() {
+  console.log("Filter activated");
+
+  //not a refreshed page
+  d3.event.preventDefault();
+  
+  //erase the current data from table
+  tbody.html("");
+
+  //take the date entered into id = "datetime"
+  var filterElement = d3.select("#datetime");
+
+  // utilize the datetime and turn it into a value to use
+  var filterValue = filterElement.property("value");
+
+  console.log(filterValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime == filterValue);
+
+  console.log(filteredData);
+
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
